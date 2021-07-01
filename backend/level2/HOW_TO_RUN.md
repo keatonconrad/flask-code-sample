780291f0f8a5db3c714a76cde5b68a0b2e8d5b31
keatonconrad <keaton.conrad@gmail.com>
2021-07-01 18:54:09 -0500

    Added implementation and tests for backend level2

---
+++ backend/level2/HOW_TO_RUN.md
@@ -0,0 +1,32 @@
+# Level 1 Solution
+
+## Explanation
+
+This API receives a payload like `data.json` and returns a response like `output.json`, per the specs in the README.
+Input validation was also added to ensure that the necessary data is included and that at least one cart is sent.
+Delivery fees are added based on cart total price.
+
+Flask was chosen as the web framework as it is extremely lightweight. Pytest was chosen as the testing library for its ease of use and readability.
+
+## Running
+
+Please install Flask using the following command:
+
+    python3 -m pip install --upgrade flask
+
+To run the web server:
+
+    export FLASK_APP=main.py
+    python3 -m flask run
+
+The server will be running on http://localhost:5000
+
+## Testing
+
+Please install pytest using the following command:
+
+    python3 -m pip install --upgrade pytest
+
+You can then run the tests for this solution with this command:
+
+    python3 -m pytest
---
+++ backend/level2/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def main():
+    data = request.get_json()
+
+    # Ensures article, cart, and delivery fee data are provided in the payload.
+    # Also ensures at least one cart is present, per the specs in the README
+    if (not all(key in data for key in ['articles', 'carts', 'delivery_fees']) or
+            len(data['carts']) < 1):
+        return {'message': 'Missing data'}, 400
+
+    # Creates a mapping of the article ids to their respective prices for easier
+    # access later
+    article_id_to_price = {x['id']: x['price'] for x in data['articles']}
+    output = []
+
+    for cart in data['carts']:
+        total = sum(article_id_to_price[item['article_id']] * item['quantity']
+                    for item in cart['items'])
+
+        for fee in data['delivery_fees']:
+            if (fee['eligible_transaction_volume']['min_price'] <= total and
+                    (fee['eligible_transaction_volume']['max_price'] is None or
+                    total < fee['eligible_transaction_volume']['max_price'])):
+                total += fee['price']
+                break
+
+        output.append({
+            'id': cart['id'],
+            'total': total
+        })
+
+    return {'carts': output}, 200
