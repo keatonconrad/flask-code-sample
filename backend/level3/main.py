bde445a08f2470e76d4da320d1444bdc7f93aed2
keatonconrad <keaton.conrad@gmail.com>
2021-07-01 19:32:28 -0500

    Refactored level3 main.py for readability

--- backend/level3/main.py
+++ backend/level3/main.py
@@ -7,28 +7,29 @@ app = Flask(__name__)
 def main():
     data = request.get_json()
 
-    # Ensures article, cart, and delivery fee data are provided in the payload.
-    # Also ensures at least one cart is present, per the specs in the README
+    # Ensures article, cart, delivery fee, and discount data are provided in the
+    # payload. Also ensures at least one cart is present, per the specs in the
+    # README
     required_data = ['articles', 'carts', 'delivery_fees', 'discounts']
     if not all(key in data for key in required_data) or len(data['carts']) < 1:
         return {'message': 'Missing data'}, 400
 
-    # Creates a mapping of the article ids to their respective prices for easier
-    # access later
-    article_id_to_price = {x['id']: x['price'] for x in data['articles']}
     output = []
 
     for cart in data['carts']:
         total = 0
 
         for item in cart['items']:
-            price = article_id_to_price[item['article_id']]
+            article = next(a for a in data['articles'] if a['id'] == item['article_id'])
+            price = article['price']
+
             discount_dict = next((d for d in data['discounts'] if d['article_id'] == item['article_id']), None)
             if discount_dict is not None:
                 if discount_dict['type'] == 'amount':
                     price -= discount_dict['value']
                 else:
                     price *= 1 - discount_dict['value'] / 100
+
             total += price * item['quantity']
 
         for fee in data['delivery_fees']:
